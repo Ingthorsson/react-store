00322a8746d4e0d49739654b86c670a77f66ac33
Stefan Ingthorsson <ingthorsson@gmail.com>
2022-05-06 19:03:57 +0000

    first commit

---
+++ src/components/Checkout.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function Checkout() {
+    return (
+        <div className="card">
+            <h2>Checkout</h2>
+            <div className="shopping-bag">
+                xx
+            </div>
+        </div>
+    )
+}
+
+export default Checkout;
